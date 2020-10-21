a0ae6b9dc5f267a1b27e0e2a9ede9a45c21b2e9e
vikto <vikto@DESKTOP-8O7VMPB>
2020-10-21 16:31:39 +0200

    Add project files.

---
+++ 10_21_Tomb/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_21_Tomb
+{
+    class Program
+    {
+        static void Paratlantomb()
+        {
+            int[] tombparatlan = new int[12];
+            int paratlanszam = 0;
+            for (int i = 0; i < 12; i++)
+            {
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
